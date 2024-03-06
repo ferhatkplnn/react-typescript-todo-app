762563e632f8c381a7a887cfff03726a0fc9d216
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-06 23:52:55 +0300

    Separate active and completed tasks in TodoList

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -12,14 +12,36 @@ const TodoList: React.FC<TodoListProps> = ({ todos, setTodos }) => {
   return (
     <div className="container">
       <div className="todo-list">
-        {todos.map((todo) => (
-          <TodoListItem
-            key={todo.id}
-            todo={todo}
-            todos={todos}
-            setTodos={setTodos}
-          />
-        ))}
+        {/* todos_heading */}
+        <span className="title">Active Tasks</span>
+        {todos.map((todo) => {
+          if (!todo.isDone) {
+            return (
+              <TodoListItem
+                key={todo.id}
+                todo={todo}
+                todos={todos}
+                setTodos={setTodos}
+              />
+            );
+          }
+        })}
+      </div>
+      {/* todos remove */}
+      <div className="todo-list remove">
+        <span className="title">Completed Tasks</span>
+        {todos.map((todo) => {
+          if (todo.isDone) {
+            return (
+              <TodoListItem
+                key={todo.id}
+                todo={todo}
+                todos={todos}
+                setTodos={setTodos}
+              />
+            );
+          }
+        })}
       </div>
     </div>
   );
