481c7b00656aeacc00a62c7e743625fc75fd0cc0
Ferhat Kaplan <fkmll2121@gmail.com>
2024-03-06 21:31:38 +0300

    Add a todo list feature.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import { useState } from "react";
 import TodoInput from "./components/TodoInput";
+import { Todo } from "./model";
 
 const App = () => {
   const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!todo) return;
+    setTodos([...todos, { id: Date.now(), isDone: false, todo }]);
+    setTodo("");
+  };
 
   return (
     <div className="App">
       <div className="heading">Todo App</div>
-      <TodoInput todo={todo} setTodo={setTodo} />
+      <TodoInput todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
     </div>
   );
 };
